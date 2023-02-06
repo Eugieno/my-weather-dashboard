481222d591f38f64357ac634b97e86f7faf5f29c
Eugene Akinbamini <eugeneakinson@gmail.com>
2023-02-06 04:19:15 +0000

    added starter html file and updated with new elemnents

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+
+// Create an event handler function that does the following; 
+// APi request to Geocoding API
+    // Get city name form form text 
+    // construct an API url with city name as a variable
+    // API ajax call to geocoding API
+    // Grab long and lat data from Geocoding response object
+        // store long and lat in variables
+    // construct ajax call to open whether API using long and latitude data for the corresponding city
+        // Append the required info on to the respective section of the front end page
+
+// add event listener to the search button of the form element
